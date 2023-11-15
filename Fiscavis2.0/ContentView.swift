dbc4258a7b4949247b764c21fd06d9117440d1f8
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-11-15 17:10:15 -0600

    Initial Commit

---
+++ Fiscavis2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fiscavis2.0
+//
+//  Created by J. DeWeese on 11/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fiscavis2.0/Fiscavis2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Fiscavis2_0App.swift
+//  Fiscavis2.0
+//
+//  Created by J. DeWeese on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Fiscavis2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
